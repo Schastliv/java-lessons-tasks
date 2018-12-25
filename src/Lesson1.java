ffcb4d148d393b06b288f66c5d9f21095940db0f
Roman <romannagula92@gmail.com>
2018-12-25 13:43:11 +0200

    Lesson 1 task 1

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Lesson1 {
+
     public static void main(String[] args) {
-       System.out.println("Lessons 1");
+        System.out.println("Я автоматизатор");
     }
 }
