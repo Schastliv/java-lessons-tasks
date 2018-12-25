25b0ff16a3f4e49bab668271d3b805420ec7e531
Roman <romannagula92@gmail.com>
2018-12-25 14:51:33 +0200

    Lesson2 task 2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -3,6 +3,7 @@ public class Lesson2 {
     public static void main (String [] arg) {
         Lesson2 lesson2 = new Lesson2();
         lesson2.firstTask();
+        lesson2.secondTask();
     }
 
     public void firstTask() {
@@ -18,4 +19,10 @@ public class Lesson2 {
         System.out.println(nums[2]);
     }
 
+    public void secondTask() {
+        int [] nums1 = {0,1,2,3,4,5};
+        int [][] nums2 = {{0,1,2}, {3,4,5}};
+        nums2[1][0] = 44;
+        System.out.println(nums2 [1][0]);
+    }
 }
