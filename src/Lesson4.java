704347665b06f193d67e7f8e707f69bbab8deb5f
Roman <romannagula92@gmail.com>
2018-12-28 00:07:00 +0200

    Lesson 4 Task 2

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -6,33 +6,34 @@ public class Lesson4 {
         Lesson4 lesson4 = new Lesson4();
         lesson4.firstTask();
         lesson4.secondTask();
+        lesson4.thirdTask();
     }
 
-        public void firstTask() {
-    int a = 60;
-    int b = 13;
-    int c = 0;
+    public void firstTask() {
+        int a = 60;
+        int b = 13;
+        int c = 0;
 
-    c = a & b;
-    System.out.println("a & b");
+        c = a & b;
+        System.out.println("a & b");
 
-    c = a | b;
-    System.out.println("a | b = " + c);
+        c = a | b;
+        System.out.println("a | b = " + c);
 
-    c = a ^ b;
-    System.out.println("a ^ b = " + c);
+        c = a ^ b;
+        System.out.println("a ^ b = " + c);
 
-    c = ~ a;
-    System.out.println("~ a = " + c);
+        c = ~a;
+        System.out.println("~ a = " + c);
 
-    c = a << 2;
-    System.out.println("a << 2 = " + c);
+        c = a << 2;
+        System.out.println("a << 2 = " + c);
 
-    c = a >> 2;
-    System.out.println("a >> 2 = " + c);
+        c = a >> 2;
+        System.out.println("a >> 2 = " + c);
 
-    c = a >>> 2;
-    System.out.println("a >>> 2 = " + c);
+        c = a >>> 2;
+        System.out.println("a >>> 2 = " + c);
 
 
     }
@@ -40,12 +41,17 @@ public class Lesson4 {
     public void secondTask() {
         int x = 3;
         int y = 4;
-System.out.println(x + y);
-System.out.println(x*y);
-System.out.println("xy");
+        System.out.println(x + y);
+        System.out.println(x * y);
+        System.out.println("xy");
 
     }
 
+    public void thirdTask() {
+for (int i = 2; i < 100; i+=2){
+    System.out.println(i + " ");
+}
 
+    }
 }
 
