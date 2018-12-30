37be1de56a6f59311376c3509ec2c67331b85f5e
Roman <romannagula92@gmail.com>
2018-12-30 18:48:07 +0200

    Lessons7 RadioButton task

--- src/Lesson7/ClassWithMain.java
+++ src/Lesson7/ClassWithMain.java
@@ -5,13 +5,13 @@ public class ClassWithMain {
         Ellement myElement = new Ellement();
         Checkbox checkbox = new Checkbox();
         Radiobutton radiobutton = new Radiobutton();
-        ImputField imputField = new ImputField();
+        InputField imputField = new InputField();
 
         Ellement []ellements = new Ellement [4];
         ellements [0] = new Ellement();
         ellements [1] = new Checkbox();
         ellements [2] = new Radiobutton();
-        ellements [3] = new ImputField();
+        ellements [3] = new InputField();
 
     }
 }
---
+++ src/Lesson7/Radiobutton.java
@@ -0,0 +1,8 @@
+package Lesson7;
+
+public class Radiobutton extends Ellement {
+    public void select()
+    {
+        System.out.print("Radiobutton selected");
+    }
+}
